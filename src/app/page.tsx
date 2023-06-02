a90da1869aaf063ed9a72aa8cbd640d9392dc435
Cauê catone silva <cauecatonesilva@gmail.com>
2023-06-02 18:14:33 -0400

    gerador de qr code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,74 @@
-import Image from 'next/image'
+"use client"
+
 import styles from './page.module.css'
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [name, setName] = useState("");
+  const [ra, setRa] = useState("");
+
+  const handleDownload = () => {
+    // Lógica para obter o link do arquivo
+    let fileLink = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${name}:${ra}`;
+    
+  //   // Criar um link de download
+    const downloadLink = document.createElement('a');
+    downloadLink.href = fileLink;
+    downloadLink.download = `${name}-Qrcode.pdf`; // Nome do arquivo para download
+
+  //   // Simular o clique no link de download
+    downloadLink.click();
+    
+  };
+
+  // e.preventDefault();
+
+  const qrCodeEncoder = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const qrCodeEncoder2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRa(e.target.value);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={styles.App}>
+    <header className={styles.header}>
+        <img src="https://www.fasipecuiaba.com.br/template/topo/logo/logotopo.png?v=5750" alt="Logo FASIPE" className={styles.logo}/>
+    </header>
+    <section className={styles.sectionContainer}>
+        <h2 className={styles.h2}> Gerar Qr code</h2>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${name}:${ra}`} alt=""  className={styles.img}/>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      {/* <a href="https://www.w3schools.com/images/myw3schoolsimage.jpg" download="proposed_file_name">Download</a> */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <form   id="inf"  className={styles.inf}>
+          <div className={styles.inf2}>
+            <p className={styles.p}>Nome:</p>
+            <input onChange={qrCodeEncoder} className={styles.input} type="text" name="name" id="name" />
+          </div>
+          <div className={styles.inf2}>
+            <p className={styles.p}>ra:</p>
+            <input onChange={qrCodeEncoder2} className={styles.input} type="text" name="ra" id="ra" />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+          </div>
+          <button 
+          onClick={handleDownload}
+          className={styles.button}
+          type="submit" >
+            Download QR code
+          </button>
+        </form>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+
+    </section>
+</div>
   )
 }
+function saveAs(url: string, teste: string) {
+  throw new Error('Function not implemented.');
+}
+
