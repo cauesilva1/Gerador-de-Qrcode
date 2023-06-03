865042fedfcfd7497837e83c0469217acdf49110
Cauê catone silva <cauecatonesilva@gmail.com>
2023-06-02 23:41:58 -0400

    Funcional gerador de QRcode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,14 @@
 
 import styles from './page.module.css'
 import { useState } from 'react';
+import axios from 'axios';
+import fs from 'fs';
 
 export default function Home() {
 
   const [name, setName] = useState("");
   const [ra, setRa] = useState("");
-
-  const handleDownload = () => {
-    // Lógica para obter o link do arquivo
-    let fileLink = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${name}:${ra}`;
-    
-  //   // Criar um link de download
-    const downloadLink = document.createElement('a');
-    downloadLink.href = fileLink;
-    downloadLink.download = `${name}-Qrcode.pdf`; // Nome do arquivo para download
-
-  //   // Simular o clique no link de download
-    downloadLink.click();
-    
-  };
+  const [converter, setConverter] = useState("");
 
   // e.preventDefault();
 
@@ -32,19 +21,43 @@ export default function Home() {
     setRa(e.target.value);
   };
 
+
+  async function converterImagemParaBase64(urlImagem: string): Promise<string> {
+    try {
+      // Fazer a requisição GET para obter a imagem
+      const resposta = await axios.get(urlImagem, { responseType: 'arraybuffer' });
+  
+      // Converter o buffer da imagem para base64
+      const imagemBase64 = Buffer.from(resposta.data).toString('base64');
+      const imagemUrl = `data:${resposta.headers['content-type']};base64,${imagemBase64}`;
+      return imagemUrl;
+    } catch (erro) {
+      console.error('Erro ao obter a imagem do servidor remoto:', erro);
+      return '';
+    }
+  }
+  
+  const urlImagem = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${name}:${ra}`;
+  converterImagemParaBase64(urlImagem).then((imagemUrl) => {
+  console.log(imagemUrl);
+  setConverter(imagemUrl)
+});
+
   return (
     <div className={styles.App}>
     <header className={styles.header}>
         <img src="https://www.fasipecuiaba.com.br/template/topo/logo/logotopo.png?v=5750" alt="Logo FASIPE" className={styles.logo}/>
     </header>
+
+
     <section className={styles.sectionContainer}>
         <h2 className={styles.h2}> Gerar Qr code</h2>
 
-      <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${name}:${ra}`} alt=""  className={styles.img}/>
+        <a href={converter} download>
+          <img src={converter} alt=""  className={styles.img}/>
+        </a>
 
-      {/* <a href="https://www.w3schools.com/images/myw3schoolsimage.jpg" download="proposed_file_name">Download</a> */}
-
-        <form   id="inf"  className={styles.inf}>
+        <div   id="inf"  className={styles.inf}>
           <div className={styles.inf2}>
             <p className={styles.p}>Nome:</p>
             <input onChange={qrCodeEncoder} className={styles.input} type="text" name="name" id="name" />
@@ -55,20 +68,14 @@ export default function Home() {
 
           </div>
           <button 
-          onClick={handleDownload}
           className={styles.button}
-          type="submit" >
-            Download QR code
+          type="button">
+            Clique na imagem para baixar o QRcode
           </button>
-        </form>
-
-
+        </div>
 
     </section>
 </div>
   )
-}
-function saveAs(url: string, teste: string) {
-  throw new Error('Function not implemented.');
 }
 
